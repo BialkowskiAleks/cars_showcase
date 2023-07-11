b4d6ec2d612090e5f582e064e802750c5b0c9c7b
John Doe <irisvoneverecc@gmail.com>
2023-07-11 12:00:10 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Footer, Navbar } from "@/components";
+
+export const metadata = {
+	title: "Car Hub",
+	description: "Discover the best cars in the world.",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className="relative">
+				<Navbar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
+}
